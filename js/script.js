34eff08f5274c0ca7a25b4a0ab34d28e3a037056
Sameh Mohamed <sameh.abdelhaleem95@gmail.com>
2021-03-13 23:54:47 +0200

    finalize static version

--- js/script.js
+++ js/script.js
@@ -15,6 +15,14 @@ $(".quote").slick({
   autoplay: true,
   autoplaySpeed: 3000,
 });
+$(".quote-2").slick({
+  prevArrow: $(".arrow_prev2"),
+  nextArrow: $(".arrow_next2"),
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+});
 $(".logos__slider").slick({
   arrows: false,
   slidesToShow: 1,
